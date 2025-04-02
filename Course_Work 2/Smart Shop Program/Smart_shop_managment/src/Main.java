30465cdaf2c16b71dc59806234faaa81e36da3cc
ReillyO-Dev <owen.j.reilly@outlook.com>
2025-04-02 11:54:37 +0100

    Update Main.java
    Adds the file reader

--- Course_Work 2/Smart Shop Program/Smart_shop_managment/src/Main.java
+++ Course_Work 2/Smart Shop Program/Smart_shop_managment/src/Main.java
@@ -5,9 +5,22 @@ import java.awt.event.ActionListener;
 public class Main {
     //Create a frame
     private static JFrame frame;
-
+    public static ArrayList<String> Products = new ArrayList<>();
 
     public static void main(String[] args) {
+        try {
+            File myObj = new File("src/Stock_List.txt");// The stock list txt file will be in a src and this will be the pathway to find it
+            Scanner myReader = new Scanner(myObj);// Opens a scanner to read the txt file
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                Products.add(data);// Adds the txt file data into the array
+                //System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");// Error handling if it cant find the file
+            e.printStackTrace();
+        }
         /*This section of code is what allows the initial creation of the frame
         and is important to the functionality of the code
          */
