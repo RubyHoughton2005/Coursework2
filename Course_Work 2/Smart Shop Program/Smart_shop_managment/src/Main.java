dc4ff3d667b8e7aa4fb7b9aeb10f2c6556a7c5ef
LukeBlaylock-Git <lukeblaylock35@gmail.com>
2025-04-02 11:50:28 +0100

    Update Main.java

--- Course_Work 2/Smart Shop Program/Smart_shop_managment/src/Main.java
+++ Course_Work 2/Smart Shop Program/Smart_shop_managment/src/Main.java
@@ -7,16 +7,60 @@ public class Main {
     private static JFrame frame;
 
 
-    public static void main(String[] args) {
-        /*This section of code is what allows the initial creation of the frame
-        and is important to the functionality of the code
-         */
+    public static void main(String[] args) { //entrypoint to code
+        initialize();
+
+
+    }
+    static void initialize(){
         frame = new JFrame("Store Management System");
         frame.setSize(800, 600);
         frame.setLayout(new FlowLayout());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
         JLabel title = new JLabel("Store Management System");
         frame.add(title);
+        
+        JButton Product = new JButton ("Product"); //"Add" Button created to frame.
+        frame.add(Product);
+        
+        JButton SalesRecord = new JButton ("Sales"); //"S"
+        frame.add(SalesRecord);
+
+        JButton InventoryManager = new JButton ("Inventory");
+        frame.add(InventoryManager);
+
+        
+        Product.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                frame.getContentPane().removeAll();
+                frame.revalidate();
+                frame.repaint();
+            }
+        });
+        SalesRecord.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                frame.getContentPane().removeAll();
+                frame.revalidate();
+                frame.repaint();
+            }
+        });
+        InventoryManager.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                frame.getContentPane().removeAll();
+                frame.revalidate();
+                frame.repaint();
+                for (int i = 0; i < Products.size(); i++){
+                    String product = Products.get(i);
+                    JLabel label = new JLabel(product);
+                    frame.add(label);
+                }
+            }
+        });
+
         frame.setVisible(true);
     }
 }
